ea234f7775ac1fd2ed2836e67afbdb441f49ebf2
Denis Romenskiy <deniskaar@gmail.com>
2022-01-18 13:34:13 +0700

    Первое задание

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 "use strict";
-let incr = 10,
-    decr = 10;
-//incr++;
-//decr--;
-console.log(incr);
-console.log(decr);
-//++incr;
-//--decr;
-console.log(incr++);
-console.log(decr--);
-console.log(++incr);
-console.log(--decr);
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+document.writeln(`Вы посмотрели ${numberOfFilms} фильмов!`);
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+for (let i = 0; i < 2; i++) {
+    let questionRes = prompt('Один из последних просмотренных фильмов?');
+    let rating = prompt('На сколько оцените его?');
+    personalMovieDB['movies'][questionRes] = rating;
+
+}
+
+document.write(JSON.stringify(personalMovieDB));
