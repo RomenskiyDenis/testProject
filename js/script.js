cce3b981836ca6a8e3e3039f71b3cea67b62907b
Denis Romenskiy <deniskaar@gmail.com>
2022-01-19 13:56:24 +0700

    Задание 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 "use strict";
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 document.writeln(`Вы посмотрели ${numberOfFilms} фильмов!`);
 let personalMovieDB = {
     count: numberOfFilms,
@@ -11,8 +11,16 @@ let personalMovieDB = {
 };
 for (let i = 0; i < 2; i++) {
     const questionRes = prompt('Один из последних просмотренных фильмов?'),
-    rating = prompt('На сколько оцените его?');
-    personalMovieDB['movies'][questionRes] = rating;
+        rating = prompt('На сколько оцените его?');
+    if (!questionRes || questionRes.length === 0 || questionRes.length > 50 || !rating) {
+        i--;
+        alert('Вы не корректно ответили на вопрос! Повторите, пожалуйста.');
+        continue;
+
+    } else {
+        personalMovieDB['movies'][questionRes] = rating;
+    }
+
 
 }
 const label = document.getElementById('label1')
