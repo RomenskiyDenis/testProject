506f7b86ad8bc23f852536457c879f600724d5fd
Denis Romenskiy <deniskaar@gmail.com>
2022-01-20 19:21:06 +0700

    Задание 3_1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 "use strict";
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-document.writeln(`Вы посмотрели ${numberOfFilms} фильмов!`);
+let numberOfFilms;
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -9,26 +8,44 @@ let personalMovieDB = {
     privat: false
 
 };
-for (let i = 0; i < 2; i++) {
-    const questionRes = prompt('Один из последних просмотренных фильмов?'),
-        rating = prompt('На сколько оцените его?');
-    if (!questionRes || questionRes.length === 0 || questionRes.length > 50 || !rating) {
-        i--;
-        alert('Вы не корректно ответили на вопрос! Повторите, пожалуйста.');
-        continue;
 
-    } else {
-        personalMovieDB['movies'][questionRes] = rating;
+//function block
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
     }
-
-
 }
-if (personalMovieDB.count < 10) {
-    alert('Просмотрено довольно мал фильмов');
-} else if (personalMovieDB.count > 30) {
-    alert('Вы киноман');
-} else {
-    alert('Вы классический зритель');
+function rememberMyFilms(){
+    for (let i = 0; i < 2; i++) {
+        const questionRes = prompt('Один из последних просмотренных фильмов?'),
+            rating = prompt('На сколько оцените его?');
+        if (!questionRes || questionRes.length === 0 || questionRes.length > 50 || !rating) {
+            i--;
+            alert('Вы не корректно ответили на вопрос! Повторите, пожалуйста.');
+            continue;
+    
+        } else {
+            personalMovieDB.movies[questionRes] = rating;
+        }
+    
+    
+    }
 }
+function detectPersonalLevel(){
+    if (personalMovieDB.count < 10) {
+        alert('Просмотрено довольно мал фильмов');
+    } else if (personalMovieDB.count > 30) {
+        alert('Вы киноман');
+    } else {
+        alert('Вы классический зритель');
+    }
+}
+//end function block
+
+start();
+rememberMyFilms();
+detectPersonalLevel();
+
 const label = document.getElementById('label1');
 label.innerHTML = JSON.stringify(personalMovieDB);
